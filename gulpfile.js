c915d0635be93b8ca2cc7ddac9750cbb53c038a3
Albert Chen <blahoink@gmail.com>
2015-02-07 13:15:20 -0500

    add serve task via gulp-nodemon

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,17 @@
 var gulp = require('gulp');
+var nodemon = require('gulp-nodemon');
+
+gulp.task('serve', function() {
+	nodemon({
+		  script: 'app.js'
+		, ext: 'html js'
+		, env: {
+			'NODE_ENV': 'development'
+		}
+	}).on('restart', function() {
+		console.log('restarted!');
+	});
+});
 
 gulp.task('default', function() {
 
