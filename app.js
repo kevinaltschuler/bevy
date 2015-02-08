bb69123a77132b025ebc1a6166ff4a41cb42cd52
Albert Chen <blahoink@gmail.com>
2015-02-07 23:49:08 -0500

    error handling

--- app.js
+++ app.js
@@ -24,11 +24,42 @@ app.use(subdomain('api', api_router));
 // load routes
 var routes = require('./routes')(app);
 
+// error handling
+// TODO: separate file?
+app.use(log_errors);
+app.use(client_error_handler);
+app.use(error_handler);
 
+function log_errors(err, req, res, next) {
+	console.error('log_errors', err.toString());
+	next(err);
+}
+function client_error_handler(err, req, res, next) {
+	console.error('client_errors', err.toString());
+	res.send(500, {
+		error: err.toString()
+	});
+	if(req.xhr) {
+		console.error(err);
+		res.send(500, {
+			error: err.toString()
+		});
+	} else {
+		next(err);
+	}
+}
+function error_handler(err, req, res, next) {
+	console.error('last_errors ', err.toString());
+	res.send(500, {
+		error: err.toString()
+	});
+}
+
+// start server
 var server = app.listen(config.app.server.port, function() {
 	var name = config.app.name;
 	var host = server.address().address;
 	var port = server.address().port;
 	var env = config.app.env;
 	console.log('%s listening at http://%s:%s in %O mode', name, host, port, env);
-})
+});
