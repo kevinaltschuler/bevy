6a2752993ef7f770f3848549cee63768ea220e65
Albert Chen <blahoink@gmail.com>
2015-02-08 21:41:42 -0500

    index function in api

--- api/users.js
+++ api/users.js
@@ -1,7 +1,26 @@
 'use strict';
 
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var User = mongoose.model('User');
+
 exports.index = function(req, res, next) {
+
+	// for now just returns all
+	//TODO: AUTH
+
+	var user_map = {};
+
+	User.find({}, function(err, users) {
+		users.forEach(function(user) {
+			user_map[user._id] = user;
+		});
+	});
+
 	res.json({
-		status: 'GET /user'
+		  status: 'GET /user'
+		, object: 'user list'
+		, users: user_map
 	});
 }
