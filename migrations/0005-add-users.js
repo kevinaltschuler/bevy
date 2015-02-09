2fe2452857b119a59c8bd80def2c548b63b6b4ca
Albert Chen <blahoink@gmail.com>
2015-02-08 21:41:22 -0500

    add user migration

--- migrations/0005-add-users.js
+++ migrations/0005-add-users.js
@@ -5,7 +5,15 @@ exports.up = function(db, next){
 
 	var users = mongodb.Collection(db, 'users');
 
-
+	users.insert({
+		  _id: 1
+		, token: ''
+		, display_name: 'foobar'
+		, password: 'foobar'
+		, email: 'foobar@example.com'
+		, created: Date.now
+		, updated: Date.now
+	}, next);
 
     next();
 };
