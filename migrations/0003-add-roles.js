aa4f0145483f76d39de92df89b62fae1ddce7f3c
Albert Chen <blahoink@gmail.com>
2015-02-08 20:47:12 -0500

    remove role migration for now

--- migrations/0003-add-roles.js
+++ migrations/0003-add-roles.js
@@ -3,18 +3,18 @@ var mongodb = require('mongodb');
 
 exports.up = function(db, next){
 
-	db.createRole({
+	/*db.createRole({
 		  role: 'admin'
 		, privileges: ['all']
 		, roles: []
-	});
+	});*/
 
     next();
 };
 
 exports.down = function(db, next){
 
-	db.dropRole('admin');
+	//db.dropRole('admin');
 
-    next();
+   next();
 };
