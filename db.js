d2fab8652f8087083b11cf4d7a75a77b75ced587
Albert Chen <blahoink@gmail.com>
2015-02-09 11:00:03 -0500

    db now first-time seeds db

--- db.js
+++ db.js
@@ -7,19 +7,45 @@
 'use strict';
 
 var mongoose = require('mongoose');
+var bcrypt = require('bcryptjs');
 var config = require('./config');
 
 // load models into mongoose
 var models = require('./models');
 
 // attempt connection
-var connection = mongoose.createConnection(config.database.URL);
+//var connection = mongoose.createConnection(config.database.URL);
+mongoose.connect(config.database.URL);
+var connection = mongoose.connection;
 connection.on('error', console.error.bind(
 	  console
 	, 'connection error:'
 ));
 connection.once('open', function() {
 	console.info('connected to database');
+
+	// seed some stuff if not already
+	var User = mongoose.model('User');
+
+	User.find(function(err, users){
+		if(err) return console.error(err);
+
+		if(users.length == 0) {
+			console.log('seeding users...');
+
+			User.create({
+				  _id: mongoose.Types.ObjectId()
+				, token: ''
+				, display_name: 'foobar'
+				, password: bcrypt.hashSync('foobar', 8)
+				, email: 'foobar@example.com'
+				, created: new Date()
+				, updated: new Date()
+			});
+		} else {
+			console.log(users);
+		}
+	});
 });
 
 
