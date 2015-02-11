e745c30089f14141e0d4e6a975c89dfc1c373be2
Albert Chen <blahoink@gmail.com>
2015-02-10 21:16:49 -0500

    dump the entire request object when generating an error

--- error.js
+++ error.js
@@ -34,12 +34,30 @@ exports.error_handler = function(err, req, res, next) {
 	}
 }
 
-exports.gen = function(message, route, type, code) {
+exports.gen = function(message, req, type, code) {
 	var obj = {};
 	obj.object = 'error';
 	if(!_.isEmpty(message)) obj.message = message;
 	else obj.message = 'default error';
-	if(!_.isEmpty(route)) obj.status = route;
+	if(!_.isEmpty(req)) {
+		obj.request = {
+			  baseUrl: req.baseUrl
+			, vars: req.body
+			, cookies: req.cookies
+			, fresh: req.fresh
+			, hostname: req.hostname
+			, remote_ip: req.ip
+			, original_url: req.originalUrl
+			, params: req.params
+			, path: req.path
+			, protocol: req.protocol
+			, query: req.query
+			, route: req.route
+			, secure: req.secure
+			, subdomains: req.subdomains
+			, xhr: req.xhr
+		}
+	}
 	if(!_.isEmpty(type)) obj.type = type;
 	if(!_.isEmpty(code)) obj.code = code;
 
