7b74e137cbf3323a9af205fb5a8ed2ffe7f8e691
Albert Chen <blahoink@gmail.com>
2015-02-10 21:04:57 -0500

    add gen error func

--- error.js
+++ error.js
@@ -1,5 +1,7 @@
 'use strict';
 
+var _ = require('underscore');
+
 exports.log_errors = function(err, req, res, next) {
 	console.error('log_errors', err.toString());
 	next(err);
@@ -18,9 +20,23 @@ exports.client_error_handler = function(err, req, res, next) {
 		next(err);
 	}
 }
+
+//broken? something about sending the same header twice
 exports.error_handler = function(err, req, res, next) {
 	console.error('last_errors ', err.toString());
 	res.send(500, {
 		error: err.toString()
 	});
+}
+
+exports.gen = function(message, route, type, code) {
+	var obj = {};
+	obj.object = 'error';
+	if(!_.isEmpty(message)) obj.message = message;
+	else obj.message = 'default error';
+	if(!_.isEmpty(route)) obj.status = route;
+	if(!_.isEmpty(type)) obj.type = type;
+	if(!_.isEmpty(code)) obj.code = code;
+
+	return obj;
 }
