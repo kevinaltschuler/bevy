8bf1d8160389f9bc64627097bf36b4f2eb0e5fe7
Albert Chen <blahoink@gmail.com>
2015-02-10 19:32:07 -0500

    plural routes for users

--- routes/api/users.js
+++ routes/api/users.js
@@ -8,10 +8,10 @@ var api = require('./../../api');
 module.exports = function(router) {
 
 	// INDEX
-	router.get('/user', api.users.index);
+	router.get('/users', api.users.index);
 
 	// CREATE
-	router.get('/user/create', api.users.create);
+	router.get('/users/create', api.users.create);
 
 	// STORE
 	//router.post('/user', function(req, res) {
@@ -21,21 +21,21 @@ module.exports = function(router) {
 	//});
 
 	// SHOW
-	router.get('/user/:id', api.users.show);
+	router.get('/users/:id', api.users.show);
 
 	// EDIT
-	router.get('/user/:id/edit', api.users.edit);
+	router.get('/users/:id/edit', api.users.edit);
 
 	// UPDATE
-	router.put('/user/:id', api.users.update);
+	router.put('/users/:id', api.users.update);
 	// browser hack
 	// TODO: disable on production
-	router.get('/user/:id/update', api.users.update);
+	router.get('/users/:id/update', api.users.update);
 
 	// DESTROY
-	router.delete('/user/:id', api.users.destroy);
+	router.delete('/users/:id', api.users.destroy);
 	// browser hack
 	// TODO: disable on production
-	router.get('/user/:id/destroy', api.users.destroy);
+	router.get('/users/:id/destroy', api.users.destroy);
 
 };
