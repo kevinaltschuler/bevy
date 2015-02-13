6ed4ef6f0e9974c2e8dced6cb7ab0e16cfa4887e
Albert Chen <blahoink@gmail.com>
2015-02-13 02:42:48 -0500

    add alias model

---
+++ models/Alias.js
@@ -0,0 +1,18 @@
+'use strict';
+
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+module.exports = new Schema({
+	  _id: Schema.Types.ObjectId // internal id
+	, users: [Schema.Types.ObjectId]
+	, name: String
+	, photos: [{ url: String }]
+	, karma: Number
+	, created: {
+		  type: Date
+		, default: Date.now }
+	, updated: {
+		  type: Date
+		, default: Date.now }
+});
