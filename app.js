0e18c298f675e24e0575d739278ad8bdf7abfe09
Albert Chen <blahoink@gmail.com>
2015-02-12 21:16:56 -0500

    restructure to allow parsing of form data

--- app.js
+++ app.js
@@ -18,6 +18,7 @@ var subdomain = require('express-subdomain');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var bodyParser = require('body-parser');
+var multer = require('multer');
 var methodOverride = require('method-override');
 var cookieParser = require('cookie-parser');
 var session = require('express-session');
@@ -33,6 +34,61 @@ var app = express();
 // connect to db
 require('./db');
 
+// pretty print json by default
+app.set('json spaces', 2);
+
+// MIDDLEWARE
+app.use(favicon('./public/img/favicon.ico'));
+
+// logger
+var access_log_stream = fs.createWriteStream(__dirname + '/log/access.log', {flags: 'a'});
+app.use(logger('dev', {stream: access_log_stream}));
+
+// form parsing
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(multer());
+app.use(methodOverride());
+
+// cookies
+app.use(cookieParser(process.env.COOKIE_SECRET));
+
+// sessions
+app.use(session({
+	  secret: 'keyboard cat'
+	, cookie: {
+		  secret: true
+		, expires: false
+	}
+	, resave: true
+	, saveUninitialized: true
+}));
+
+// passport
+config.passport(app);
+app.use(passport.initialize());
+app.use(passport.session());
+
+// disable csurf until we need it
+//app.use(csrf());
+//app.use(function(req, res, next) {
+//	res.locals.csrf = req.csrfToken();
+//	return next();
+//});
+
+// cors
+app.use(middleware.cors());
+
+// templating engine for pages outside of the SPA
+app.set('view engine', 'jade');
+app.set('views', './views');
+
+// error handling
+var error = require('./error');
+app.use(error.log_errors);
+app.use(error.client_error_handler);
+app.use(error.error_handler);
+
 
 // load api routes onto api.bevy subdomain
 // TODO: user auth
@@ -44,59 +100,8 @@ app.use(subdomain('api', api_router));
 // load routes
 var routes = require('./routes')(app);
 
-// middleware
-app.use(favicon('./public/img/favicon.ico'));
-
-// pretty print json by default
-// disable in production?
-app.set('json spaces', 2);
-
-var access_log_stream = fs.createWriteStream(__dirname + '/log/access.log', {flags: 'a'});
-app.use(logger('dev', {stream: access_log_stream}));
-
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-	extended: true
-}));
-app.use(methodOverride());
-app.use(cookieParser(process.env.COOKIE_SECRET));
-app.use(session({
-	  secret: 'keyboard cat'
-	, cookie: {
-		  secret: true
-		, expires: false
-	}
-	, resave: true
-	, saveUninitialized: true
-}));
-
-var User = mongoose.model('User');
-config.passport(app, User);
-app.use(passport.initialize());
-app.use(passport.session());
-
-
-app.use(csrf());
-app.use(function(req, res, next) {
-	res.locals.csrf = req.csrfToken();
-	return next();
-});
-
-// cors
-app.use(middleware.cors());
-
-// templating engine for pages outside of the SPA
-app.set('view engine', 'jade');
-app.set('views', './views');
-
 // static directories
 app.use(express.static(__dirname + '/public'));
-
-// error handling
-var error = require('./error');
-app.use(error.log_errors);
-app.use(error.client_error_handler);
-app.use(error.error_handler);
 
 
 // start server
