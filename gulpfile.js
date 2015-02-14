78399f4e04b8e6f5c918ec45263299b2e0de6ad5
Albert Chen <blahoink@gmail.com>
2015-02-14 13:00:30 -0500

    add gulp automation

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,10 @@
 var gulp = require('gulp');
+
 var nodemon = require('gulp-nodemon');
+var browserify = require('browserify');
+var reactify = require('reactify');
+var browserSync = require('browser-sync');
+var source = require('vinyl-source-stream');
 
 gulp.task('serve', function() {
 	nodemon({
@@ -13,6 +18,27 @@ gulp.task('serve', function() {
 	}).on('restart', function() {
 		console.log('restarted!');
 	});
+});
+
+gulp.task('compile', function() {
+	var b = browserify();
+	b.transform(reactify);
+	b.add('./public/js/index.js');
+	return b.bundle()
+		.pipe(source('app.js'))
+		.pipe(gulp.dest('./public/js/build'));
+});
+
+gulp.task('browser-sync', function() {
+	browserSync({
+		server: {
+			base: './'
+		}
+	});
+});
+
+gulp.task('watch', function() {
+	gulp.watch(["*.js", "*.jsx"], ['compile', browserSync.reload]);
 });
 
 gulp.task('default', function() {
