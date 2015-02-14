fb46a9874a6bc264efdc2b8b2ccbbf58464eb918
Albert Chen <blahoink@gmail.com>
2015-02-14 13:22:51 -0500

    storemixin helper

---
+++ public/js/shared/helpers/storeMixin.js
@@ -0,0 +1,20 @@
+// mixin to help components listen to stores in a simple way
+// the component needs to implement 'onStoreUpdate' to set the state
+// [events="add remove reset change"]
+
+'use strict';
+
+module.exports = function(store, events) {
+	if(!events) events = "add remove reset change";
+
+	return {
+		componentDidMount: function() {
+			store.on(events, function() {
+				this.forceUpdate();
+			}, this);
+		},
+		componentWillUnmount: function() {
+			store.off(null, null, this);
+		}
+	};
+}
