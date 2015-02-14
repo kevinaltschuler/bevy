c246d9c93b3f59bcb3783f9c855fed0ad89727f6
Albert Chen <blahoink@gmail.com>
2015-02-14 13:49:34 -0500

    app bootstrap

---
+++ public/js/app/components/App.jsx
@@ -0,0 +1,12 @@
+'use strict';
+
+var React = require('react');
+
+module.exports = React.createClass({
+	render: function(){
+		return <div>
+		the app
+		</div>;
+
+	}
+});
--- public/js/index.js
+++ public/js/index.js
@@ -1,3 +1,10 @@
 'use strict';
 
 console.log('app loaded');
+
+require('./shared/polyfills/Object.assign.js');
+
+var React = require('react');
+var App = require('./app/components/App.jsx');
+
+React.render(<App />, document.getElementById('app'));
