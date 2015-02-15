614281eda10ba508f2598dc30e34a0d22adfa6d8
Albert Chen <blahoink@gmail.com>
2015-02-15 12:38:58 -0500

    router test link

--- public/js/app/components/App.jsx
+++ public/js/app/components/App.jsx
@@ -1,11 +1,13 @@
 'use strict';
 
 var React = require('react');
+var RouterLink = require('./../../router/components/RouterLink.jsx');
 
 module.exports = React.createClass({
 	render: function(){
 		return <div>
 		the app
+		<RouterLink href="test">test</RouterLink>
 		</div>;
 
 	}
