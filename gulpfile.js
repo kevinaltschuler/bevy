5fe39c390d4f41b48aa853336edc7d8ced335838
Albert Chen <blahoink@gmail.com>
2015-02-15 11:21:40 -0500

    watchify integration

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,41 @@
 var gulp = require('gulp');
+var gutil = require('gulp-util');
+var sourcemaps = require('gulp-sourcemaps');
 
 var nodemon = require('gulp-nodemon');
 var browserify = require('browserify');
+var to5ify = require('6to5ify');
 var reactify = require('reactify');
+var watchify = require('watchify');
 var browserSync = require('browser-sync');
 var source = require('vinyl-source-stream');
+var buffer = require('vinyl-buffer');
+
+gulp.task('js', bundle); // so you can run `gulp js` to build the file
+
+function bundle() {
+	var b = browserify({
+		  cache: {}
+		, packageCache: {}
+		, fullPaths: true
+	});
+	b = watchify(b);
+	b.on('update', function() {
+		bundleShare(b);
+	});
+	b.transform(reactify);
+	b.transform(to5ify);
+	b.add('./public/js/index.js');
+	bundleShare(b);
+}
+
+function bundleShare(b) {
+	console.log('updating...');
+	b.bundle({ debug: true })
+		.on('error', gutil.log.bind(gutil, 'Browserify Error'))
+		.pipe(source('bundle.js'))
+		.pipe(gulp.dest('./public/js/build'));
+}
 
 gulp.task('serve', function() {
 	nodemon({
@@ -20,26 +51,9 @@ gulp.task('serve', function() {
 	});
 });
 
-gulp.task('compile', function() {
-	var b = browserify();
-	b.transform(reactify);
-	b.add('./public/js/index.js');
-	return b.bundle()
-		.pipe(source('app.js'))
-		.pipe(gulp.dest('./public/js/build'));
-});
-
-gulp.task('browser-sync', function() {
-	browserSync({
-		server: {
-			base: './'
-		}
-	});
-});
-
-gulp.task('watch', function() {
-	gulp.watch(["*.js", "*.jsx"], ['compile', browserSync.reload]);
-});
+//gulp.task('watch', function() {
+//	gulp.watch(["*.js", "*.jsx"], ['compile', browserSync.reload]);
+//});
 
 gulp.task('default', function() {
 
