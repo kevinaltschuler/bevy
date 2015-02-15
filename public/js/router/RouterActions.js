1e0fadef4012d8bd3bfc65056a24c8e7765cf44b
Albert Chen <blahoink@gmail.com>
2015-02-15 12:39:33 -0500

    router actions

---
+++ public/js/router/RouterActions.js
@@ -0,0 +1,16 @@
+'use strict';
+
+var constants = require('./constants');
+var dispatch = require('./../shared/helpers/dispatch');
+
+module.exports = {
+	navigate: function(fragment, trigger, replace) {
+		console.log('navigating to...', fragment);
+		dispatch(constants.ROUTE_NAVIGATE, {
+			  fragment: fragment
+			, trigger: (trigger == undefined) ? true : trigger
+			, replace: (replace == undefined) ? true : replace
+		});
+	}
+};
+
---
+++ public/js/router/constants.js
@@ -0,0 +1,5 @@
+'use strict'
+
+module.exports = {
+	ROUTE_NAVIGATE: 'ROUTE_NAVIGATE'
+};
