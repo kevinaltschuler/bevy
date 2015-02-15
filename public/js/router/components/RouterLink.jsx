55f744fec01de02bc8023b3cc4ed11d2965c4199
Albert Chen <blahoink@gmail.com>
2015-02-15 12:39:44 -0500

    router link component

---
+++ public/js/router/components/RouterLink.jsx
@@ -0,0 +1,15 @@
+'use strict';
+
+var React = require('react');
+var RouterActions = require('./../RouterActions');
+
+module.exports = React.createClass({
+	navigate: function(ev) {
+		ev.preventDefault();
+		RouterActions.navigate(ev.target.getAttribute('href'), true);
+	},
+
+	render: function() {
+		return <a {...this.props} onClick={this.navigate} />
+	}
+});
