8ad38f7fad99192478f42195c096923599570ea7
Albert Chen <blahoink@gmail.com>
2015-02-17 16:03:58 -0500

    remove display name (belongs to alias

--- models/User.js
+++ models/User.js
@@ -7,7 +7,6 @@ module.exports = new Schema({
 	, open_id: String // openid from third-party service, if applicable
 	, provider: String // third-party service connected to
 	, token: String
-	, display_name: String
 	, password: String
 	, email: String
 	, aliases: [Schema.Types.ObjectId]
