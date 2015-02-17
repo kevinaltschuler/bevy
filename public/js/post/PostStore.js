2aff313b32e013ea7aecf6f598562edcd9ecc8a7
Albert Chen <blahoink@gmail.com>
2015-02-17 17:28:17 -0500

    add defaults

--- public/js/post/PostStore.js
+++ public/js/post/PostStore.js
@@ -1,1 +1,19 @@
 'use strict';
+
+var Backbone = require('backbone');
+var _ = require('underscore');
+
+var Post = Backbone.Model.extend({
+	defaults: {
+		  image_url: null
+		, comment_count: 0
+		, point_count: 0
+		, created: new Date()
+		, updated: new Date()
+		, author: null
+		, comments: []
+		, bevy: null
+		, title: null
+		, body: null
+	}
+});
