ffbbf4cd9a5aee0b6f7f0827ad41612bbebdb22f
Albert Chen <blahoink@gmail.com>
2015-02-17 16:05:44 -0500

    unique flag for openid

--- models/User.js
+++ models/User.js
@@ -4,7 +4,10 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 module.exports = new Schema({
-	, open_id: String // openid from third-party service, if applicable
+	, open_id: {
+		  type: String // openid from third-party service, if applicable
+		, unique: true
+	}
 	, provider: String // third-party service connected to
 	, token: String
 	, password: String
