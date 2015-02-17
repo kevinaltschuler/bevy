f97732509acf2e2f56fdeeae338431958e036bb9
Albert Chen <blahoink@gmail.com>
2015-02-17 17:29:46 -0500

    import post store

--- public/js/post/components/PostContainer.jsx
+++ public/js/post/components/PostContainer.jsx
@@ -4,6 +4,8 @@ var React = require('react');
 
 var Post = require('./Post.jsx');
 
+var PostStore = require('./../PostStore');
+
 module.exports = React.createClass({
 	render: function() {
 		return	<div className="col-xs-6">
