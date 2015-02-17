aff480d8331faae210715f5b024df242c1edd7e6
Albert Chen <blahoink@gmail.com>
2015-02-17 16:23:12 -0500

    formatting

--- public/js/app/components/PostSubmit.jsx
+++ public/js/app/components/PostSubmit.jsx
@@ -4,15 +4,15 @@ var React = require('react');
 
 module.exports = React.createClass({
 	render: function() {
-		return <div className="col-xs-12">
-		          <div className="input-group">
-		            <input type="text" className="form-control" placeholder=" "/>
-		            <span className="input-group-btn">
-		              <button className="btn btn-default" data-toggle="tooltip" data-placement="right" title="" data-original-title="add media to your post" type="button">
-		              	+
-		              </button>
-		            </span>
-		          </div>
-		        </div>;
+		return	<div className="col-xs-12">
+						<div className="input-group">
+							<input type="text" className="form-control" placeholder=" "/>
+							<span className="input-group-btn">
+								<button className="btn btn-default" data-toggle="tooltip"
+								data-placement="right" title="" data-original-title="add media to your post" type="button">+
+								</button>
+							</span>
+						</div>
+					</div>;
 	}
 });
