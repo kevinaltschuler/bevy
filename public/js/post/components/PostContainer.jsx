209be1019a121ae8cc22e109018286fd65e71996
Albert Chen <blahoink@gmail.com>
2015-02-18 15:47:32 -0500

    post container doc

--- public/js/post/components/PostContainer.jsx
+++ public/js/post/components/PostContainer.jsx
@@ -1,5 +1,13 @@
+/**
+ * PostContainer.jsx
+ * React Component that manages and wraps around
+ * Post panels
+ * @author albert
+ */
+
 'use strict';
 
+// imports
 var React = require('react');
 var ReactPropTypes = React.PropTypes;
 
@@ -7,8 +15,11 @@ var Post = require('./Post.jsx');
 var PostStore = require('./../PostStore');
 
 
+// React class
 var PostContainer = React.createClass({
 
+	// expects App.jsx to pass in Posts collection
+	// see App.jsx and PostStore.js for more details
 	propTypes: {
 		allPosts: ReactPropTypes.array.isRequired
 	},
@@ -21,14 +32,20 @@ var PostContainer = React.createClass({
 
 		if(Object.keys(this.props.allPosts).length < 1) {
 			// no posts
-			return <div className="col-xs-6" />;
+			// still return column so the app retains its structure
+			return	<div className="col-xs-6">
+							Nothing to see here...
+						</div>;
 		}
 
+		// load props into local vars
 		var allPosts = this.props.allPosts;
 		var posts = [];
 
+		// for each post
 		for(var key in allPosts) {
 			var post = allPosts[key];
+			// load post into array
 			posts.push(<Post title = { post.title }
 				body = { post.body } image_url = { post.image_url }
 				author = { post.author } bevy = { post.bevy } />);
@@ -40,4 +57,5 @@ var PostContainer = React.createClass({
 	}
 });
 
+// pipe back to App.jsx
 module.exports = PostContainer;
