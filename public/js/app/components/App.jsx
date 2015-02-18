c8a691754cd29bac6b3b88190aa6a6a3b2d75251
Albert Chen <blahoink@gmail.com>
2015-02-18 15:41:27 -0500

    app documentation

--- public/js/app/components/App.jsx
+++ public/js/app/components/App.jsx
@@ -1,42 +1,61 @@
+/**
+ * App.jsx
+ * the main react component of the app. the entry point within the entry point
+ * @author albert
+ */
+
 'use strict';
 
+// imports
 var React = require('react');
+
 var RouterLink = require('./../../router/components/RouterLink.jsx');
 var Router = require('./../../router/components/Router.jsx');
 
 var Header = require('./Header.jsx');
 var Navbar = require('./Navbar.jsx');
-var PostSubmit = require('./../../post/components/PostSubmit.jsx');
-var PostSort = require('./../../post/components/PostSort.jsx');
+
 var LeftSidebar = require('./LeftSidebar.jsx');
 var RightSidebar = require('./RightSidebar.jsx')
-var PostContainer = require('./../../post/components/PostContainer.jsx');
 
+var PostSubmit = require('./../../post/components/PostSubmit.jsx');
+var PostSort = require('./../../post/components/PostSort.jsx');
+var PostContainer = require('./../../post/components/PostContainer.jsx');
 var PostStore = require('./../../post/PostStore');
 
 
-
+/**
+ * update posts by getting the collection from the store
+ * @return [post_obj] collection of post models - refer to PostStore.js for more details
+ */
 function getPostState() {
 	return {
 		allPosts: PostStore.getAll()
 	}
 }
 
-module.exports = React.createClass({
+// create app
+var App = React.createClass({
 
+	// called directly after mounting
 	getInitialState: function() {
+		// init posts
 		PostStore.initialize();
 		return getPostState();
+
 	},
 
+	// mount event listeners
 	componentDidMount: function() {
 		PostStore.on('change', this._onPostChange);
 	},
 
+	// unmount event listeners
 	componentWillUnmount: function() {
 		PostStore.off('change', this._onPostChange);
 	},
 
+	// event listener callbacks
 	_onPostChange: function() {
 		this.setState(getPostState());
 	},
@@ -60,3 +79,6 @@ module.exports = React.createClass({
 					</div>;
 	}
 });
+
+// pipe back to index.js
+module.exports = App;
