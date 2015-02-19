bec2be1e801cf4e07bb944c8b8065ada0192842f
Albert Chen <blahoink@gmail.com>
2015-02-19 15:40:46 -0500

    trigger sort action

--- public/js/post/components/PostSort.jsx
+++ public/js/post/components/PostSort.jsx
@@ -6,17 +6,18 @@ var PostActions = require('./../PostActions');
 
 module.exports = React.createClass({
 
-	sort: function() {
-		PostActions.sort();
+	sort: function(ev) {
+		var by = ev.target.textContent;
+		PostActions.sort(by);
 	},
 
 	render: function() {
 		return	<div className="sort-well">
 						<div className="col-xs-12 btn-group btn-group-sort" role="group">
 							<text className="btn-group-text">
-								<button type="button" className="sort-btn btn active" id="top-btn">top</button>
+								<button type="button" className="sort-btn btn active" id="top-btn" onClick={ this.sort }>top</button>
 								•
-								<button type="button" className="sort-btn btn" id="new-btn">new</button>
+								<button type="button" className="sort-btn btn" id="new-btn" onClick={ this.sort }>new</button>
 							</text>
 						</div>
 					</div>;
