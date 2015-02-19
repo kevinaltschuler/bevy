6548812a846a88b5331b815d9cabe969163e9302
kevtastic <kevinaltschuler@gmail.com>
2015-02-18 23:01:58 -0500

    text posts

---
+++ public/js/post/components/TextPost.jsx
@@ -0,0 +1,84 @@
+/**
+ * Post.jsx
+ * React class for an individual post
+ * Created en masse by PostContainer.jsx
+ * @author albert
+ */
+
+'use strict';
+
+// imports
+var React = require('react');
+var ReactPropTypes = React.PropTypes;
+
+var mui = require('material-ui');
+var IconButton = mui.IconButton;
+
+var PostActions = require('./../PostActions');
+
+// React class
+var Post = React.createClass({
+
+	// expects (most) of these to be passed in by PostContainer.jsx
+	propTypes: {
+		  id: ReactPropTypes.number
+		, title: ReactPropTypes.string
+		, body: ReactPropTypes.string
+		, image_url: ReactPropTypes.string
+		, author: ReactPropTypes.string
+		, bevy: ReactPropTypes.string
+		, comments: ReactPropTypes.array
+		, points: ReactPropTypes.array
+	},
+
+	defaults: {
+		// no need to set these, as they've already been set
+		// by PostActions.js
+	},
+
+	getInitialState: function() {
+		return {};
+	},
+
+	upvote: function(ev) {
+		ev.preventDefault();
+		PostActions.upvote(this.props.id, this.props.author);
+	},
+
+	downvote: function(ev) {
+		ev.preventDefault();
+		PostActions.downvote(this.props.id, this.props.author);
+	},
+
+	render: function() {
+		return	<div className="panel" postId={ this.props.id }>
+						<div className="panel-heading">
+							<a href={ this.props.image_url }>{ this.props.title }</a>
+						</div>
+						<div className="panel-details">{ this.props.author } • { this.props.bevy } • 12 hours ago</div>
+						<div className="panel-body-text" tabIndex="0">
+							Nuts Nuts Nuts Nuts Nuts Nuts Nuts Nuts Nuts Nuts Nuts Nuts Nuts Nuts Nuts 
+							Nuts Nuts Nuts Nuts Nuts Nuts Nuts Nuts Nuts Nuts Nuts Nuts Nuts Nuts Nuts 
+							Nuts Nuts Nuts Nuts Nuts Nuts Nuts Nuts Nuts Nuts Nuts Nuts Nuts Nuts Nuts 
+							Nuts Nuts Nuts Nuts Nuts Nuts Nuts Nuts Nuts Nuts Nuts Nuts Nuts Nuts Nuts 
+						</div>
+						<div className="panel-commments"></div>
+						<div className="panel-bottom">
+							<div className="panel-controls-left">
+								{ this.props.points.length } points<br/>{ this.props.comments.length } comments
+							</div>
+							<div className="panel-controls-right">
+								<IconButton tooltip='upvote'>
+									<span className="glyphicon glyphicon-menu-up btn" onClick={ this.upvote }></span>
+								</IconButton>
+								<IconButton tooltip='downvote'>
+									<span className="glyphicon glyphicon-menu-down btn" onClick={ this.downvote }></span>
+								</IconButton>
+								<span className="glyphicon glyphicon-option-vertical btn"></span>
+							</div>
+						</div>
+					</div>
+	}
+});
+
+module.exports = Post;
