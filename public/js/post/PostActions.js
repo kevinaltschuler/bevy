c81b8db2f145e14521b5299635bc7d4fd0e47512
Albert Chen <blahoink@gmail.com>
2015-02-18 20:28:21 -0500

    postaction doc

--- public/js/post/PostActions.js
+++ public/js/post/PostActions.js
@@ -1,10 +1,25 @@
+/**
+ * PostActions.js
+ *
+ * the glue between the front end React components
+ * and the back end Backbone models
+ *
+ * uses the helper dispatch function for clarity of
+ * event name
+ *
+ * @author albert
+ */
+
 'use strict';
 
+// imports
 var dispatch = require('./../shared/helpers/dispatch');
 
+
 var PostActions = {
+	// create a post
 	create: function(title, body, image_url, author, bevy) {
-		console.log('creating post action...');
+		//console.log('creating post action...');
 		dispatch('create', {
 			  title: (title == undefined) ? 'untitled' : title
 			, body: (body == undefined) ? 'nothing here' : body
