db1bf19efb2c6caf1537810a094d7d24dedcb59d
Albert Chen <blahoink@gmail.com>
2015-02-18 20:05:03 -0500

    less watch fix?

--- gulpfile.js
+++ gulpfile.js
@@ -51,7 +51,7 @@ function bundleShare(b) {
 }
 
 gulp.task('less', function() {
-	var stream = gulp.src('public/less/app.less')
+	var stream = gulp.src('public/less/*.less')
 		.pipe(watch('public/less/*.less'))
 		.pipe(less())
 		// now do css transformations
