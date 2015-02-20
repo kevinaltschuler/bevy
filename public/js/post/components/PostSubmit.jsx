3d07d161ddec8e7396d26d8c374e6d72b546edf9
Albert Chen <blahoink@gmail.com>
2015-02-19 22:32:47 -0500

    postsubmit doc

--- public/js/post/components/PostSubmit.jsx
+++ public/js/post/components/PostSubmit.jsx
@@ -1,5 +1,14 @@
+/**
+ * PostSubmit.jsx
+ *
+ * The dialog for creating a post
+ *
+ * @author albert
+ */
+
 'use strict';
 
+// imports
 var React = require('react');
 var OverlayTrigger = require('react-bootstrap').OverlayTrigger;
 var Tooltip = require('react-bootstrap').Tooltip;
@@ -7,29 +16,39 @@ var Input = require('react-bootstrap').Input;
 
 var PostActions = require('./../PostActions');
 
+// React class
 var PostSubmit = React.createClass({
 
-
+	// trigger the create action
+	// TODO: pass in the rest of the state attributes needed
 	submit: function() {
 		PostActions.create(this.state.title);
 	},
 
+	// start with an empty title
+	// TODO: when the dialog is expanded, add the default options here
 	getInitialState: function() {
 		return {
 			title: ''
 		};
 	},
 
+	// used to trigger the create action (enter key)
+	// later, we can use this to listen for ctrl+v and other media shortcuts
 	onKeyUp: function(ev) {
 		//if the user hits enter, submit a new post
 		if(ev.which === 13) {
 			this.submit();
+			// empty the current title attribute
+			// in case the user wants to enter another right quick
 			this.setState({
 				title: ''
 			});
 		}
 	},
 
+	// triggered every time a key is pressed
+	// updates the state
 	handleChange: function() {
 		this.setState({
 			title: this.refs.input.getValue()
