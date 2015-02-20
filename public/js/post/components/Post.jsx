1c339bece66eff0ce57ab5eb5d53e6ba60a5d84c
Albert Chen <blahoink@gmail.com>
2015-02-20 15:09:16 -0500

    revert post.jsx to working state

--- public/js/post/components/Post.jsx
+++ public/js/post/components/Post.jsx
@@ -13,14 +13,9 @@ var ReactPropTypes = React.PropTypes;
 
 var mui = require('material-ui');
 var IconButton = mui.IconButton;
-var Paper = mui.Paper;
 
 var PostActions = require('./../PostActions');
 var PostStore = require('./../PostStore');
-
-var rbs = require('react-bootstrap');
-var Input = rbs.Input;
-var Panel = rbs.Panel;
 
 var $ = require('jquery');
 
@@ -125,17 +120,64 @@ var Post = React.createClass({
 	render: function() {
 		//console.log(this.state);
 		//TODO: fix if logic
+		//if(true) {
 		return	<div className="panel" postId={ this.state.id }>
-					<Input type="text" placeholder="Title" />
-  					<Input type="textarea" placeholder="Body"/>
-					<div className="panel-bottom">
-						<div className="panel-controls-right">
-							<IconButton iconClassName="glyphicon glyphicon-plus" tooltip="attach media"/>
-							<IconButton iconClassName="glyphicon glyphicon-send" tooltip="Post" onClick={PostSubmit.submit}/>
+						<div className="panel-heading">
+							<a href={ this.state.image_url }>{ this.state.title }</a>
+						</div>
+						<div className="panel-details">{ this.state.author } • { this.state.bevy } • { this.timeAgo() }</div>
+						<div className="panel-body" tabIndex="0">
+							<img className="panel-media" src={ this.state.image_url }/>
+						</div>
+						<div className="panel-commments"></div>
+						<div className="panel-bottom">
+							<div className="panel-controls-left">
+								{ this.countVotes() } points<br/>{ this.state.comments.length } comments
+							</div>
+							<div className="panel-controls-right">
+								<IconButton tooltip='upvote'>
+									<span className="glyphicon glyphicon-menu-up btn" onClick={ this.upvote }></span>
+								</IconButton>
+								<IconButton tooltip='downvote'>
+									<span className="glyphicon glyphicon-menu-down btn" onClick={ this.downvote }></span>
+								</IconButton>
+								<span className="glyphicon glyphicon-option-vertical btn"></span>
+							</div>
 						</div>
 					</div>
-				</div>
-				}
+		//		}
+		/*else {
+			return  <div className="panel" postId={ this.state.id }>
+						<div className="panel-heading">
+							<a href={ this.props.image_url }>{ this.props.title }</a>
+						</div>
+						<div className="panel-details">{ this.props.author } • { this.props.bevy } • 12 hours ago</div>
+						<div className="panel-body panel-body-text" tabIndex="0" >
+							Nuts nuts nuts nuts nuts nuts nuts nuts nuts nuts nuts
+							nuts nuts nuts nuts nuts nuts nuts nuts nuts nuts nuts
+							nuts nuts nuts nuts nuts nuts nuts nuts nuts nuts nuts nuts
+							nuts nuts nuts nuts nuts nuts nuts nuts nuts nuts nuts nuts nuts nuts nuts
+							nuts nuts nuts nuts nuts nuts nuts nuts nuts nuts nuts nuts
+							nuts nuts nuts nuts nuts nuts nuts nuts nuts
+						</div>
+						<div className="panel-commments"></div>
+						<div className="panel-bottom">
+							<div className="panel-controls-left">
+								{ this.countVotes() } points<br/>{ this.props.comments.length } comments
+							</div>
+							<div className="panel-controls-right">
+								<IconButton tooltip='upvote'>
+									<span className="glyphicon glyphicon-menu-up btn" onClick={ this.upvote }></span>
+								</IconButton>
+								<IconButton tooltip='downvote'>
+									<span className="glyphicon glyphicon-menu-down btn" onClick={ this.downvote }></span>
+								</IconButton>
+								<span className="glyphicon glyphicon-option-vertical btn"></span>
+							</div>
+						</div>
+					</div>
+		}*/
+	}
 });
 
 module.exports = Post;
