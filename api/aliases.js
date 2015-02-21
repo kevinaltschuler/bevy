898a33c8d42c911f8846611458501a9556e9aa20
Albert Chen <blahoink@gmail.com>
2015-02-21 12:19:51 -0500

    clean start on alias api

--- api/aliases.js
+++ api/aliases.js
@@ -13,81 +13,14 @@ var Alias = mongoose.model('Alias');
 // INDEX
 // GET /users/:id/aliases/
 exports.index = function(req, res, next) {
-	var user_id = req.params.id;
-	var user_object_id = ObjectId.createFromHexString(user_id);
 
-	var user_query = { _id: user_object_id };
-	var user_promise = User.findOne(user_query).exec();
-	user_promise.then(function(user) {
-		if(!user) {
-			throw error.gen('user not found', req);
-		}
-
-		var aliases = user.aliases;
-		res.json({
-			  status: 'GET /user/' + user_id + '/aliases/'
-			, object: 'alias array'
-			, aliases: aliases
-		});
-	}).then(null, function(err) {
-		next(err);
-	});
 }
 
 // CREATE
 // GET /users/:id/aliases/create
 // POST /users/:id/aliases
 exports.create = function(req, res, next) {
-	var user_id = req.params.id;
-	var user_object_id = ObjectId.createFromHexString(user_id);
 
-	var user_query = { _id: user_object_id };
-	var user_promise = User.findOne(user_query).exec();
-	user_promise.then(function(user) {
-		if(!user) {
-			throw error.gen('user not found', req);
-		}
-		return user;
-	}).then(
-	function(user) {
-		var update = {};
-
-		Alias.schema.eachPath(function(pathname, schema_type) {
-			// collect path value
-			var val = null;
-			if(req.body != undefined) val = req.body[pathname];
-			if(!val && !_.isEmpty(req.query)) val = req.query[pathname];
-			if(!val) return;
-
-			update[pathname] = val;
-		});
-
-		if(!update.name) throw error.gen('alias name not specified', req);
-
-		update._id = new ObjectId();
-		update.created = new Date();
-		update.updated = new Date();
-
-		Alias.create(update, function(err, alias) {
-			if(err) throw err;
-
-			user.aliases.push(alias);
-			user.save(function(err) {
-				if(err) throw err;
-			});
-
-			res.json({
-				  status: 'GET /users/' + user_id + '/aliases/create'
-				, object: 'alias'
-				, alias: alias
-			});
-		});
-	},
-	function(err) {
-		next(err);
-	}).then(null, function(err) {
-		next(err);
-	});
 }
 
 // SHOW
--- routes/api/aliases.js
+++ routes/api/aliases.js
@@ -6,29 +6,29 @@ var api = require('./../../api');
 module.exports = function(router) {
 
 	// INDEX
-	router.get('/users/:id/aliases', api.aliases.index);
+	router.get('/aliases', api.aliases.index);
 
 	// CREATE
-	router.get('/users/:id/aliases/create', api.aliases.create);
+	router.get('/aliases/create', api.aliases.create);
 
 	// STORE
-	router.post('/users/:id/aliases', api.aliases.create);
+	router.post('/aliases', api.aliases.create);
 
 	// SHOW
-	router.get('/users/:id/aliases/:name', api.aliases.show);
+	router.get('/aliases/:name', api.aliases.show);
 
 	// EDIT
-	router.get('/users/:id/aliases/:name/edit', api.aliases.edit);
+	router.get('/aliases/:name/edit', api.aliases.edit);
 
 	// UPDATE
-	router.put('/users/:id/aliases/:name', api.aliases.update);
+	router.put('/aliases/:name', api.aliases.update);
 	// browser hack
 	// TODO: disable on production
-	router.get('/users/:id/aliases/:name/update', api.aliases.update);
+	router.get('/aliases/:name/update', api.aliases.update);
 
 	// DESTROY
-	router.delete('/users/:id/aliases/:name', api.aliases.destroy);
+	router.delete('/aliases/:name', api.aliases.destroy);
 	// browser hack
 	// TODO: disable on production
-	router.get('/users/:id/aliases/:name/destroy', api.aliases.destroy);
+	router.get('/aliases/:name/destroy', api.aliases.destroy);
 }
