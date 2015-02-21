ec533d303e00868f35e18511c3b5656a2526efae
Albert Chen <blahoink@gmail.com>
2015-02-20 20:46:03 -0500

    bevies api structure

---
+++ api/bevies.js
@@ -0,0 +1,31 @@
+'use strict';
+
+// INDEX
+exports.index = function(req, res, next) {
+
+}
+
+// CREATE
+exports.create = function(req, res, next) {
+
+}
+
+// SHOW
+exports.show = function(req, res, next) {
+
+}
+
+// EDIT
+exports.edit = function(req, res, next) {
+
+}
+
+// UPDATE
+exports.update = function(req, res, next) {
+
+}
+
+// DESTROY
+exports.destroy = function(req, res, next) {
+
+}
---
+++ routes/api/bevies.js
@@ -0,0 +1,29 @@
+'use strict';
+
+// load api functions
+var api = require('./../../api');
+
+module.exports = function(router) {
+
+	// INDEX
+	router.get('/bevies', api.bevies.index);
+
+	// CREATE
+	router.get('/bevies/create', api.bevies.create);
+	router.post('/bevies', api.bevies.create);
+
+	// SHOW
+	router.get('/bevies/:id', api.bevies.show);
+
+	// EDIT
+	router.get('/bevies/:id/edit', api.bevies.edit);
+
+	// UPDATE
+	router.get('/bevies/:id/update', api.bevies.update);
+	router.put('/bevies/:id', api.bevies.update);
+
+	// DESTROY
+	router.get('/bevies/:id/destroy', api.bevies.destroy);
+	router.delete('/bevies/:id', api.bevies.destroy);
+
+};
