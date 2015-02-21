621d840e3ef539557253fc2754f332c4c72efbdf
Albert Chen <blahoink@gmail.com>
2015-02-20 19:13:00 -0500

    style fix:

--- public/js/app/components/RightSidebar.jsx
+++ public/js/app/components/RightSidebar.jsx
@@ -12,16 +12,16 @@ var MenuItem = rbs.MenuItem;
 var $ = require('jquery');
 
 module.exports = React.createClass({
-      render: function() {
-      	return  <ButtonGroup className="col-sm-2 hidden-xs btn-group right-sidebar">
-                  <DropdownButton className="dropdown-button" title='Notifications'>
-                    <MenuItem eventKey="1">Action</MenuItem>
-                  </DropdownButton>
-                  <Badge>2</Badge>
-                    <br/>
-                  <DropdownButton className="dropdown-button" title='Invites'>
-                    <MenuItem eventKey="1">Action</MenuItem> 
-                  </DropdownButton>
-                </ButtonGroup>;
-              }
+	render: function() {
+		return	<ButtonGroup className="col-sm-2 hidden-xs btn-group right-sidebar">
+						<DropdownButton className="dropdown-button" title='Notifications'>
+							<MenuItem eventKey="1">Action</MenuItem>
+						</DropdownButton>
+						<Badge>2</Badge>
+						<br/>
+						<DropdownButton className="dropdown-button" title='Invites'>
+							<MenuItem eventKey="1">Action</MenuItem>
+						</DropdownButton>
+					</ButtonGroup>;
+	}
 });
