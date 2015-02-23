485e07f33e98f7bd18010782be433d93cabe62fe
Albert Chen <blahoink@gmail.com>
2015-02-23 17:42:15 -0500

    posts api index

--- api/posts.js
+++ api/posts.js
@@ -13,12 +13,27 @@ var _ = require('underscore');
 
 var Post = mongoose.model('Post');
 
+// INDEX
+// GET /bevies/:bevyid/posts
 exports.index = function(req, res, next) {
+	var bevy_id = req.params.bevyid;
 
+	var query = { bevy: bevy_id };
+	var promise = Post.find(query)
+		.populate('bevy')
+		.populate('comments')
+		.exec();
+	promise.then(function(posts) {
+		res.json({
+			  status: 'INDEX BEVY ' + bevy_id + ' POSTS'
+			, object: 'post array'
+			, posts: posts
+		});
+	}, function(err) { next(err); });
 }
 
 exports.create = function(req, res, next) {
-
+	var bevy_id = req.params.bevyid;
 }
 
 exports.show = function(req, res, next) {
