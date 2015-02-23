e5abb287cd0c7affd1ba7dd121c037e10db1ec02
kevtastic <kevinaltschuler@gmail.com>
2015-02-23 16:14:02 -0500

    updateing login page

--- public/js/login/components/RegisterPage.jsx
+++ public/js/login/components/RegisterPage.jsx
@@ -11,10 +11,7 @@ var Button = rbs.Button;
 
 var RegisterPage = React.createClass({
 	render: function() {
-		return	<div className="login-container">
-					<div className="login-header row">
-						<img src="../../../img/logo_100.png" height="60" width="60"/>
-					</div>
+		return	<div className="login-container register">
 					<div className="login-title row">
 						<h1>Create a New Account</h1>
 					</div>
