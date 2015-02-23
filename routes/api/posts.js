a53b42f404f369f69e3d5dfb1c7fee9538789892
Albert Chen <blahoink@gmail.com>
2015-02-23 17:19:34 -0500

    posts as a subresource of bevies

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -14,24 +14,24 @@ var api = require('./../../api');
 module.exports = function(router) {
 
 	// INDEX
-	router.get('/posts', api.posts.index);
+	router.get('/bevies/:bevyid/posts', api.posts.index);
 
 	// CREATE
-	router.get('/posts/create', api.posts.create);
-	router.post('/posts', api.posts.create);
+	router.get('/bevies/:bevyid/posts/create', api.posts.create);
+	router.post('/bevies/:bevyid/posts', api.posts.create);
 
 	// SHOW
-	router.get('/posts/:id', api.posts.show);
+	router.get('/bevies/:bevyid/posts/:id', api.posts.show);
 
 	// EDIT
-	router.get('/posts/:id/edit', api.posts.update);
+	router.get('/bevies/:bevyid/posts/:id/edit', api.posts.update);
 
 	// UPDATE
-	router.get('/posts/:id/update', api.posts.update);
-	router.put('/posts/:id', api.posts.update);
+	router.get('/bevies/:bevyid/posts/:id/update', api.posts.update);
+	router.put('/bevies/:bevyid/posts/:id', api.posts.update);
 
 	// DESTROY
-	router.get('/posts/:id/destroy', api.posts.destroy);
-	router.delete('/posts/:id', api.posts.destroy);
+	router.get('/bevies/:bevyid/posts/:id/destroy', api.posts.destroy);
+	router.delete('/bevies/:bevyid/posts/:id', api.posts.destroy);
 
 }
