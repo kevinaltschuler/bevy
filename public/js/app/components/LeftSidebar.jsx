8cffcad928230f2f96be3956cfc53aa494b76f66
Albert Chen <blahoink@gmail.com>
2015-02-25 21:04:04 -0500

    style ocd

--- public/js/app/components/LeftSidebar.jsx
+++ public/js/app/components/LeftSidebar.jsx
@@ -14,27 +14,18 @@ var mui = require('material-ui');
 var IconButton = mui.IconButton;
 
 module.exports = React.createClass({
-      render: function() {
-        return <ButtonGroup className="col-sm-3 hidden-xs btn-group left-sidebar" role="group">
-                <text className="btn-group-text">
-                  <Button type="button" className="sort-btn btn active">Front Page</Button>
-                  <br/>
-                    <Button type="button" className="sort-btn btn">New England Melee</Button>
-                  <br/>
-                  <Button type="button" className="sort-btn btn">
-                    Burlap <Badge>12</Badge>
-                  </Button>
-                  <br/>
-                  <Button type="button" className="sort-btn btn">
-                    Neu Frisbee <Badge>4</Badge>
-                  </Button> 
-                  <br/>
-                  <Button type="button" className="sort-btn btn">Bevy Team</Button>
-                  <br/>
-                  <ModalTrigger modal={<CreateNewBevy />}>
-                    <IconButton iconClassName="glyphicon glyphicon-plus" tooltip="Create new bevy"/>
-                  </ModalTrigger>
-                </text>
-               </ButtonGroup>;
-             }
+	render: function() {
+		return	<ButtonGroup className="col-sm-3 hidden-xs btn-group left-sidebar" role="group">
+						<text className="btn-group-text">
+							<Button type="button" className="sort-btn btn active">Front Page</Button><br/>
+							<Button type="button" className="sort-btn btn">New England Melee</Button><br/>
+							<Button type="button" className="sort-btn btn">Burlap <Badge>12</Badge></Button><br/>
+							<Button type="button" className="sort-btn btn">Neu Frisbee <Badge>4</Badge></Button><br/>
+							<Button type="button" className="sort-btn btn">Bevy Team</Button><br/>
+							<ModalTrigger modal={<CreateNewBevy />}>
+								<IconButton iconClassName="glyphicon glyphicon-plus" tooltip="Create new bevy"/>
+							</ModalTrigger>
+						</text>
+					</ButtonGroup>;
+	}
 });
