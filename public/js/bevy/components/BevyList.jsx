f9540628aeac9d9547aabf7b6e3a1dab264bb310
Albert Chen <blahoink@gmail.com>
2015-02-26 20:23:05 -0500

    bevy list structure

---
+++ public/js/bevy/components/BevyList.jsx
@@ -0,0 +1,21 @@
+/**
+ * BevyList.jsx
+ *
+ * List of bevies
+ *
+ * @author albert
+ */
+
+'use strict';
+
+var React = require('react');
+
+var BevyList = React.createClass({
+
+	render: function() {
+
+	}
+
+});
+
+module.exports = BevyList;
