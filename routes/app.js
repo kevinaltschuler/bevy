91b583118ac04c6b134d226e7b6d442bc0a3db14
Albert Chen <blahoink@gmail.com>
2015-03-05 01:30:45 -0500

    login page first

--- routes/app.js
+++ routes/app.js
@@ -1,7 +1,20 @@
 'use strict';
 
+var passport = require('passport');
+
 module.exports = function(app) {
-	app.get('/', function(req, res, next) {
+	app.get('/', require_user, function(req, res, next) {
 		res.render('app', { env: process.env.NODE_ENV });
 	});
+}
+
+function require_user(req, res, next) {
+	if(req.user) {
+		console.log(req.user);
+		next();
+	}
+	else {
+		res.redirect('/login');
+	}
+	//next();
 }
