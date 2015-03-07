0cd23eaa91afd03a685fb4fa1ebdea47ac0c4908
Albert Chen <blahoink@gmail.com>
2015-03-07 17:26:15 -0500

    add and config mailgun

--- config/index.js
+++ config/index.js
@@ -7,7 +7,9 @@
 var app = require('./app');
 var database = require('./database');
 var passport = require('./passport');
+var mailgun = require('./mailgun');
 
 exports.app = app;
 exports.database = database;
 exports.passport = passport;
+exports.mailgun = mailgun;
---
+++ config/mailgun.js
@@ -0,0 +1,23 @@
+/**
+ * mailgun.js
+ *
+ *	configure the mailgun wrapper
+ *
+ * @author albert
+ */
+
+'use strict';
+
+// TODO: load this via mailgun API?
+var api_key = 'key-1abe5c471492adc2e07014a1ea3add45';
+// TODO: base this off of constants siteurl?
+var domain = 'mg.bvy.io';
+
+
+module.exports = function() {
+	return require('mailgun-js')({
+		  apiKey: api_key
+		, domain: domain
+	});
+}
+
