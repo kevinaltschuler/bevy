276f7edf05f49c07d2ac7f760f7fa59c0f1a1acb
Albert Chen <blahoink@gmail.com>
2015-03-08 16:53:34 -0400

    add credential header and options verb

--- middleware/cors.js
+++ middleware/cors.js
@@ -3,8 +3,9 @@
 module.exports = function(req, res, next) {
 	// TODO: restrict cors domains
 	res.header('Access-Control-Allow-Origin', '*');
-	res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-	res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, X-Access-Token, X-Revision, Content-Type, Accept');
+	res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+	res.header('Access-Control-Allow-Credentials', 'true');
+	res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, X-Requested-By, X-Access-Token, X-Revision, Content-Type, Accept');
 
 	next();
 }
