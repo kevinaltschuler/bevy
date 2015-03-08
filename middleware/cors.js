00943824d2184528a2d2b71cc193b29b03142156
Albert Chen <blahoink@gmail.com>
2015-03-08 17:37:48 -0400

    style

--- middleware/cors.js
+++ middleware/cors.js
@@ -3,7 +3,7 @@
 module.exports = function(req, res, next) {
 	// TODO: restrict cors domains
 	res.header('Access-Control-Allow-Origin', '*');
-	res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+	res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE, OPTIONS');
 	res.header('Access-Control-Allow-Credentials', 'true');
 	res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, X-Requested-By, X-Access-Token, X-Revision, Content-Type, Accept');
 
