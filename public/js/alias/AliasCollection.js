ed9a9bf902ec8a9bbe4fd0224ca3cfd9881a25c8
Albert Chen <blahoink@gmail.com>
2015-03-13 20:45:09 -0400

    user dependent alias url

--- public/js/alias/AliasCollection.js
+++ public/js/alias/AliasCollection.js
@@ -10,14 +10,19 @@
 
 // imports
 var Backbone = require('backbone');
+var _ = require('underscore');
+
 var Alias = require('./AliasModel');
 var constants = require('./../constants');
 
 // backbone collection
 module.exports = Backbone.Collection.extend({
 	  model: Alias
-	, url: constants.apiurl + '/aliases'
+	, url: function() {
+		return (_.isEmpty(this._meta.bevyid)) ? constants.apiurl + '/aliases'
+		: constants.apiurl + '/users/' + this._meta.userid + '/aliases'
+	}
 	, _meta: {
-
+		userid: null
 	}
 });
