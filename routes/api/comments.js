8b2cae296d94e703f2302dad2af6e8bcc68f1af7
Albert Chen <blahoink@gmail.com>
2015-03-14 15:48:11 -0400

    add non-bevy dependent routes

--- routes/api/comments.js
+++ routes/api/comments.js
@@ -30,4 +30,27 @@ module.exports = function(router) {
 	// DESTROY
 	router.get('/bevies/:bevyid/posts/:postid/comments/:id/destroy', api.comments.destroy);
 	router.delete('/bevies/:bevyid/posts/:postid/comments/:id', api.comments.destroy);
+
+	// non-bevy dependent routes
+
+	// INDEX
+	router.get('/posts/:postid/comments', api.comments.index);
+
+	// CREATE
+	router.get('/posts/:postid/comments/create', api.comments.create);
+	router.post('/posts/:postid/comments', api.comments.create);
+
+	// SHOW
+	router.get('/posts/:postid/comments/:id', api.comments.show);
+
+	// EDIT
+	router.get('/posts/:postid/comments/:id/edit', api.comments.update);
+
+	// UPDATE
+	router.get('/posts/:postid/comments/:id/update', api.comments.update);
+	router.put('/posts/:postid/comments/:id', api.comments.update);
+
+	// DESTROY
+	router.get('/posts/:postid/comments/:id/destroy', api.comments.destroy);
+	router.delete('/posts/:postid/comments/:id', api.comments.destroy);
 }
