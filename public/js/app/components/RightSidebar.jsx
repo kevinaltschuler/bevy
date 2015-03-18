9611a880aaf7cc53d5c340b5918818470dcb3018
Albert Chen <blahoink@gmail.com>
2015-03-18 12:56:38 -0400

    style

--- public/js/app/components/RightSidebar.jsx
+++ public/js/app/components/RightSidebar.jsx
@@ -36,41 +36,40 @@ module.exports = React.createClass({
 
 
 		return	<ButtonGroup className="col-sm-3 hidden-xs btn-group right-sidebar panel">
-					<div className="row sidebar-top">
-						<div className="col-xs-3 sidebar-picture">
-							<img src={ defaultBevyImage }/>
+						<div className="row sidebar-top">
+							<div className="col-xs-3 sidebar-picture">
+								<img src={ defaultBevyImage }/>
+							</div>
+							<div className="col-xs-9 sidebar-title">
+								<span className='sidebar-title-name'>{ bevyName }</span>
+								<span className='sidebar-title-description'>The Frontpage</span>
+							</div>
 						</div>
-						<div className="col-xs-9 sidebar-title">
-							<span className='sidebar-title-name'>{ bevyName }</span>
-							<span className='sidebar-title-description'>The Frontpage</span>
+
+						<div className='row sidebar-links'>
+							<ButtonGroup className="col-xs-12" role="group">
+								<Button type='button' className="sidebar-link">
+									Invite People
+								</Button>
+								•
+								<Button type='button' className="sidebar-link">
+									21 Members
+								</Button>
+							</ButtonGroup>
 						</div>
-					</div>
 
-					<div className='row sidebar-links'>
-						<ButtonGroup className="col-xs-12" role="group">
-							<Button type='button' className="sidebar-link">
-								Invite People
+						<div className='row sidebar-action'>
+							<div className="sidebar-action-title col-xs-12"> Currently Viewing </div>
+							<Button type='button' className="sidebar-action-link">
+								Nobody
 							</Button>
-							•
-							<Button type='button' className="sidebar-link">
-								21 Members
-							</Button>
-						</ButtonGroup>
-					</div>
-
-					<div className='row sidebar-action'>
-						<div className="sidebar-action-title col-xs-12"> Currently Viewing </div>
-						<Button type='button' className="sidebar-action-link">
-							Nobody
-						</Button>
-					</div>
-					<div className='row sidebar-action'>
-						<div className="sidebar-action-title col-xs-12"> Notifications </div>
-						<DropDownMenu menuItems={notificationMenuItems} />
-					</div>
+						</div>
+						<div className='row sidebar-action'>
+							<div className="sidebar-action-title col-xs-12"> Notifications </div>
+							<DropDownMenu menuItems={notificationMenuItems} />
+						</div>
 
 
-				</ButtonGroup>
-				;
+					</ButtonGroup>
 	}
 });
