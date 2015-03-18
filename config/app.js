70cad7544ec5393ab2b23e66f1803b661d42eb03
Albert Chen <blahoink@gmail.com>
2015-03-18 00:08:16 -0400

    distinction between dev and prod server vars

--- config/app.js
+++ config/app.js
@@ -27,12 +27,21 @@ exports.log = {
 	path: __dirname + "../log/app_#{currentEnv}.log"
 };
 
-exports.server = {
-	  port: 80
-	  // In staging and production, listen loopback. nginx listens on the network.
-	, ip: '127.0.0.1'
-	, hostname: 'http://bevy.dev'
-};
+if(current_env == 'development') {
+	exports.server = {
+		  port: 80
+		  // In staging and production, listen loopback. nginx listens on the network.
+		, ip: '127.0.0.1'
+		, hostname: 'http://bevy.dev'
+	};
+} else {
+	exports.server = {
+		  port: 80
+		  // In staging and production, listen loopback. nginx listens on the network.
+		, ip: '127.0.0.1'
+		, hostname: 'http://bvy.io'
+	};
+}
 
 /*exports.ssl_opts = {
 	  key: fs.readFileSync(keys_dir + 'server.key')
@@ -42,9 +51,9 @@ exports.server = {
 	, agent: false
 };*/
 
-if (current_env != 'production' && current_env != 'staging') {
+/*if (current_env != 'production' && current_env != 'staging') {
 	exports.enableTests = true;
 	// Listen on all IPs in dev/test (for testing from other machines)
 	exports.server.ip = '0.0.0.0';
 	exports.server.hostname = 'http://bevy.dev';
-};
+};*/
