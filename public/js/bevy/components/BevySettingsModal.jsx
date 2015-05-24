cd68d06ba8dafe8731a56ec8a07cd1f1a2a2ac0e
kevtastic <kevinaltschuler@gmail.com>
2015-05-24 17:56:20 -0400

    panmel polish and settings

--- public/js/bevy/components/BevySettingsModal.jsx
+++ public/js/bevy/components/BevySettingsModal.jsx
@@ -20,24 +20,33 @@ var FlatButton = mui.FlatButton;
 var RaisedButton = mui.RaisedButton;
 var TextField = mui.TextField;
 var IconButton = mui.IconButton;
+var Toggle = mui.Toggle;
 
 var BevyActions = require('./../BevyActions');
 
 var BevySettingsModal = React.createClass({
 
 	propTypes: {
-		activeBevy: React.PropTypes.object,
+		activeBevy: React.PropTypes.object
 	},
 
 	render: function() {
 
 		return <Modal className="bevy-settings-modal">
-					<span className="title">Settings for {activeBevy.get('name')}</span>
+					<span className="title">Settings for {this.props.activeBevy.get('name')}</span>
+
+					<div className='row bevy-setting'>
+						<Toggle
+						  name="toggleName1"
+						  value="toggleValue1"
+						  label="allow users to change display name?" />
+					</div>
 
 					<div className='row'>
 						<div className='col-xs-12'>
 							<div className="panel-bottom">
 								<RaisedButton
+									onClick={this.props.onRequestHide}
 									label="Save"/>
 							</div>
 						</div>
