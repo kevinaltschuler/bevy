df578d75a8f0cea8d99bab4af09a63276265114f
Albert Chen <blahoink@gmail.com>
2015-06-04 15:32:43 -0400

    smaller images

--- public/js/profile/components/ProfileDropdown.jsx
+++ public/js/profile/components/ProfileDropdown.jsx
@@ -45,6 +45,7 @@ var ProfileDropdown = React.createClass({
 		//console.log(file);
 		var filename = file.filename;
 		var image_url = constants.apiurl + '/files/' + filename;
+		image_url += '?w=100&h=100';
 		this.setState({
 			image_url: image_url
 		});
