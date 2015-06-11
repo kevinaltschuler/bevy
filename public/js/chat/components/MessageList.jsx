b16b0e9563e6b37bb87b718f0cea8f3f93d79b45
Albert Chen <blahoink@gmail.com>
2015-06-11 17:36:08 -0400

    fix scroll reset after closing

--- public/js/chat/components/MessageList.jsx
+++ public/js/chat/components/MessageList.jsx
@@ -17,9 +17,21 @@ var MessageList = React.createClass({
 		return {};
 	},
 
-	componentDidUpdate: function() {
+	componentDidMount: function() {
 		var node = this.getDOMNode();
 		node.scrollTop = node.scrollHeight;
+	},
+
+	componentWillUpdate: function() {
+		var node = this.getDOMNode();
+		this.shouldScrollBottom = node.scrollTop + node.offsetHeight === node.scrollHeight;
+	},
+
+	componentDidUpdate: function() {
+		if(this.shouldScrollBottom) {
+			var node = this.getDOMNode();
+			node.scrollTop = node.scrollHeight;
+		}
 	},
 
 	render: function() {
