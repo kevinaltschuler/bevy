1b7d80beaafb5c021ab23cc70309d6342ef13984
Albert Chen <blahoink@gmail.com>
2015-08-12 19:51:40 -0400

    auth folder cleanup

--- public/js/auth/components/RegisterPage.jsx
+++ public/js/auth/components/RegisterPage.jsx
@@ -13,22 +13,20 @@ var React = require('react');
 var RegisterPanel = require('./RegisterPanel.jsx')
 
 var RegisterPage = React.createClass({
-	render: function() {
-		return <div className='register-container'>
-
-					<div className='register-title'>
-						<h1>Create a New Account</h1>
-					</div>
-
-					<RegisterPanel />
-
-					<div className='back-link'>
-						<a href='/login'>Back to Login</a>
-					</div>
-
-					<br/>
-				 </div>
-	}
+  render() {
+    return (
+      <div className='register-container'>
+        <div className='register-title'>
+          <h1>Create a New Account</h1>
+        </div>
+        <RegisterPanel />
+        <div className='back-link'>
+          <a href='/login'>Back to Login</a>
+        </div>
+        <br/>
+      </div>
+    );
+  }
 });
 
 module.exports = RegisterPage;
