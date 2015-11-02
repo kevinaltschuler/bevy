1ae71503c2e4e50d410dab71113646aad5ffbac9
Albert Chen <blahoink@gmail.com>
2015-11-02 18:51:20 -0500

    sub to kitty cats only when user id is set

--- socket.js
+++ socket.js
@@ -16,8 +16,7 @@ module.exports = function(server) {
     var subSock = zmq.socket('sub');
     subSock.connect('tcp://127.0.0.1:4000');
     //console.log('new subscriber bound to port 4000');
-
-    subSock.subscribe('kitty cats');
+    //
     subSock.on('message', function(event, data) {
       event = event.toString();
       data = data.toString();
@@ -27,6 +26,7 @@ module.exports = function(server) {
 
     socket.on('set_user_id', function(user_id) {
       console.log('user_id', user_id);
+      subSock.subscribe('kitty cats');
       subSock.subscribe('chat:' + user_id);
       subSock.subscribe('notification:' + user_id);
     });
