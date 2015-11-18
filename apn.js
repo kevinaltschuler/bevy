5f0e02398a6778d08379f05f2909d488ffbf0e8c
Albert Chen <blahoink@gmail.com>
2015-11-18 12:05:36 -0500

    kevin sux

--- apn.js
+++ apn.js
@@ -6,6 +6,8 @@
 
 var apn = require('apn');
 var zmq = require('zmq');
+var gcm = require('node-gcm');
+var gcm_sender = new gcm.Sender('AIzaSyAwwjrZ_RkwmCFx5Gs8ENKQvVABgZ22W4g');
 
 var options = { };
 var apnConnection = new apn.Connection(options);
@@ -18,44 +20,35 @@ subSock.subscribe('chat_message');
 
 //listener for a new chat message
 subSock.on('message', function(event, data) {
-    var thread = data.thread;
-    var author = data.author;
-    console.log('event: ', event);
-    console.log('data: ', data);
+  event = event.toString();
+  data = JSON.parse(data.toString());
+  var message = data.message;
+  var to_users = data.to_users;
+  var thread = message.thread;
+  var author = message.author;
+  
+  //for all users in a thread
+  for(var i in to_users) {
+    var user = to_users[i];
+    // send a notification to all devices
+    for(var j in user.devices) {
+      var device = user.devices[j];
+      console.log('sending to ', user.displayName, ' ', device.id);
 
-    if(thread == undefined || author  == undefined) {
-    	console.log('thread or author are undefined', 'thread: ', thread, 'author: ', author);
-    	return;
+      if(device.platform == 'ios') {
+        var iosDevice = new apn.Device(device.token);
+        var note = new apn.Notification();
+
+        note.expiry = Math.floor(Date.now() / 1000) + 3600; // Expires 1 hour from now.
+        note.badge = 3;
+        note.sound = "ping.aiff";
+        note.alert = "\uD83D\uDCE7 \u2709 You have a new message";
+        note.payload = {'messageFrom': author.displayName};
+
+        apnConnection.pushNotification(note, iosDevice);
+      } else if (device.platform == 'android') {
+        
+      }
     }
-
-    console.log('2');
-    //for all users in a thread
-	for(var key in thread.users) {
-
-		var user = thread.users[key];
-		// if theyre the author, then dont send a notification
-		if(user == author._id)  {
-			return;
-		}
-		else {
-			// send a notification to all devices
-			for(var key in user.devices) {
-				var device = user.devices[key];
-				// only send to ios for now
-				if(device.platform == 'ios') {
-					console.log('sending to ', user.displayName, ' ', device.id);
-					var iosDevice = new apn.Device(device.token);
-					var note = new apn.Notification();
-
-					note.expiry = Math.floor(Date.now() / 1000) + 3600; // Expires 1 hour from now.
-					note.badge = 3;
-					note.sound = "ping.aiff";
-					note.alert = "\uD83D\uDCE7 \u2709 You have a new message";
-					note.payload = {'messageFrom': author.displayName};
-
-					apnConnection.pushNotification(note, iosDevice);
-				}
-			}
-		}
-	}
+  }
 });
