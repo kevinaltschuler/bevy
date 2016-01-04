9a618001ad5aa4fa0d84a89c7aed9e92e08e0cb9
Albert Chen <blahoink@gmail.com>
2016-01-04 09:24:42 -0500

    notification item styling

--- public/js/notification/components/NotificationItem.jsx
+++ public/js/notification/components/NotificationItem.jsx
@@ -1,34 +1,33 @@
 /**
  * NotificationItem.jsx
  *
- * @author KEVIN
+ * @author kevin
  * @author albert
+ * @flow
  */
 
+'use strict';
+
 var React = require('react');
-var $ = require('jquery');
-var _ = require('underscore');
+var {
+  ButtonGroup,
+  Button,
+  Panel
+} = require('react-bootstrap');
+var {
+  IconButton
+} = require('material-ui');
 var Ink = require('react-ink');
 
+var _ = require('underscore');
+var $ = require('jquery');
 var router = require('./../../router');
-
-var rbs = require('react-bootstrap');
-var ButtonGroup = rbs.ButtonGroup;
-var Button = rbs.Button;
-var Panel = rbs.Panel;
-
-var mui = require('material-ui');
-var IconButton = mui.IconButton;
-
 var NotificationActions = require('./../NotificationActions');
 var BevyActions = require('./../../bevy/BevyActions');
-
 var timeAgo = require('./../../shared/helpers/timeAgo');
-
 var user = window.bootstrap.user;
 
 var NotificationItem = React.createClass({
-
   propTypes: {
     id: React.PropTypes.string,
     event: React.PropTypes.string,
@@ -41,24 +40,7 @@ var NotificationItem = React.createClass({
     NotificationActions.dismiss(this.props.id);
   },
 
-  join(ev) {
-    ev.preventDefault();
-
-    var data = this.props.data;
-    var bevy_id = data.bevy_id;
-    var email = user.email;
-
-    NotificationActions.read(this.props.id);
-    BevyActions.join(bevy_id, window.bootstrap.user, email);
-    //BevyActions.switchBevy();
-  },
-
-  acceptJoin(ev) {
-    ev.preventDefault();
-  },
-
   render() {
-
     var event = this.props.event;
     var data = this.props.data;
     var read = this.props.read;
@@ -72,47 +54,7 @@ var NotificationItem = React.createClass({
     };
 
     var body;
-
     switch(event) {
-      case 'invite:email':
-        //var bevy = data.bevy;
-        //var from_user = data.from_user;
-        var bevy_name = data.bevy_name;
-        var bevy_img = data.bevy_img;
-        var inviter_name = data.inviter_name;
-        console.log(data);
-
-        var imgStyle = (bevy_img == undefined)
-        ? {display: 'none'}
-        : {
-          backgroundImage: 'url(' + bevy_img + ')',
-        };
-
-        body = (
-          <Button className="notification-body">
-            <div className="sidebar-picture" style={imgStyle}/>
-            <div className=" notification-text-col">
-              <div className="notification-text">
-                <div className="name">
-                  { inviter_name }
-                </div>
-              </div>
-              <div className="notification-text">
-                <div className="content">
-                  Invite to { bevy_name }
-                </div>
-              </div>
-            </div>
-            <Button
-              className="accept-btn"
-              onClick={ this.join } >
-              Join
-            </Button>
-          </Button>
-        );
-
-        break;
-
       case 'post:create':
         var author_name = data.author_name;
         var author_image = data.author_image;
@@ -131,10 +73,11 @@ var NotificationItem = React.createClass({
 
         body = (
           <Button
-            href={ '/b/' + bevy_id + '/post/' + post_id }
+            href={ '/boards/' + board_id + '/post/' + post_id }
             className="notification-body"
             onClick={ goToPost }
           >
+            <Ink />
             <div className="sidebar-picture" style={ imgStyle }/>
             <div className=" notification-text-col">
               <span>
@@ -151,47 +94,6 @@ var NotificationItem = React.createClass({
             </div>
           </Button>
         );
-
-        break;
-
-      case 'bevy:requestjoin':
-        var user_id = data.user_id;
-        var user_name = data.user_name;
-        var user_image = data.user_image;
-        var bevy_id = data.bevy_id;
-        var bevy_name = data.bevy_name;
-        var imgStyle = (user_image == undefined)
-        ? { display: 'none' }
-        : { backgroundImage: 'url(' + user_image.path + ')' };
-
-        body = (
-          <div className='notification-body'>
-            <div className=' sidebar-picture' style={imgStyle}/>
-            <div className=' notification-text-col'>
-              <div className='row'>
-                <span>Request to join <b>{ bevy_name }</b></span>
-              </div>
-              <div className='row'>
-                <span>From <i>{ user_name }</i></span>
-              </div>
-            </div>
-            <Button
-              className="accept-btn"
-              style={{position: 'relative'}}
-              onClick={ this.dismiss } >
-              Dismiss
-              <Ink style={{ color: '#aaa', width: '85px', height: '100%'}}/>
-            </Button>
-            <Button
-              className="accept-btn"
-              style={{position: 'relative'}}
-              onClick={ this.acceptJoin } >
-              Accept
-              <Ink style={{ color: '#aaa', width: '85px', height: '100%'}}/>
-            </Button>
-          </div>
-        );
-
         break;
 
       case 'post:reply':
@@ -206,6 +108,7 @@ var NotificationItem = React.createClass({
 
         body = (
           <Button className='notification-body' >
+            <Ink />
             <div className='sidebar-picture' style={ imgStyle }/>
             <div className='notification-text-col'>
               <b>{ author_name }</b>
@@ -234,6 +137,7 @@ var NotificationItem = React.createClass({
 
         body = (
           <Button className='notification-body' >
+            <Ink />
             <div className='sidebar-picture' style={ imgStyle }/>
             <div className='notification-text-col'>
               <b>{ author_name }</b>
@@ -258,9 +162,8 @@ var NotificationItem = React.createClass({
     }
 
     return (
-      <Panel className="notification-item" style={itemStyle}>
+      <Panel className="notification-item" style={ itemStyle }>
         { body }
-
       </Panel>
     );
   }
