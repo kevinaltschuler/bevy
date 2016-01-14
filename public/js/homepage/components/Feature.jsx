41886500a5c8ce4ca6b1ade345055a5952c6a693
kevin <kevinaltschuler@gmail.com>
2016-01-13 23:09:56 -0500

    NOICE FRONT

---
+++ public/js/homepage/components/Feature.jsx
@@ -0,0 +1,48 @@
+/**
+ * a feature on the homepage 
+ */
+
+'use strict';
+
+var React = require('react');
+
+var Feature = React.createClass({
+	propTypes: {
+		title: React.PropTypes.string,
+		onClick: React.PropTypes.func,
+		description: React.PropTypes.description
+	},
+
+	_handleClick() {
+		this.props.onClick();
+	},
+
+	render() {
+
+		var selectedStyle = (this.props.selected == this.props.index)
+		? {
+			backgroundColor: 'rgba(44,182,115,.12)'
+		}
+		: {}
+
+		var selectedTextStyle = (this.props.selected == this.props.index)
+		? { color: '#2cb673'}
+		: {}
+
+		return (
+			<div style={selectedStyle} className='feature' onClick={this._handleClick}>
+              <div style={selectedTextStyle} className='top'>
+                {this.props.icon}
+                <div  className='title'>
+                  {this.props.title}
+                </div>
+              </div>
+              <div className='description'>
+                { this.props.description }
+              </div>
+            </div>
+		);
+	}
+});
+
+module.exports = Feature;
--- public/js/homepage/components/HomeView.jsx
+++ public/js/homepage/components/HomeView.jsx
@@ -15,27 +15,76 @@ var {
 } = require('material-ui');
 
 var CreateNewBevyModal = require('./../../bevy/components/CreateNewBevyModal.jsx');
+var Feature = require('./Feature.jsx');
 
 var _ = require('underscore');
 var router = require('./../../router');
 
+var $ = require('jquery');
+var TYPED = require('./typed.js').TYPED;
+
+
 var HomeView = React.createClass({
   getInitialState() {
     return {
-      showNewBevyModal: false
+      showNewBevyModal: false,
+      selected: 0
     };
   },
 
+  componentDidMount() {
+    TYPED($);
+    $(function(){
+        $(".typedSpan").typed({
+            strings: ["Fraternity.", "Sorority.", "Non Profit.", "Club.", "Business.", "Community."],
+            typeSpeed: 50,
+            startDelay: 1000,
+            backDelay: 800,
+            backSpeed: 5
+        });
+    })
+  },
+
   render() {
+
+    var boardScreenStyle = (this.state.selected == 0)
+    ? {
+      backgroundImage: "url('./../../../img/boardscreen.png')",
+      marginLeft: "-540px",
+      marginTop: "70px",
+      backgroundSize: "auto 100%"
+    }
+    : {
+      display: 'none',
+    }
+
+    var chatScreenStyle = (this.state.selected == 1)
+    ? {
+      backgroundImage: "url('./../../../img/chatscreen.png')",
+      marginLeft: "-140px",
+      marginTop: "50px",
+      backgroundSize: "auto 100%"
+    }
+    : {display: 'none'}
+
+    var feedScreenStyle = (this.state.selected == 2)
+    ? {
+      backgroundImage: "url('./../../../img/postscreen.png')",
+      marginLeft: "-400px",
+      marginTop: "70px",
+      backgroundSize: "auto 100%"
+    }
+    : {display: 'none'}
+
     return (
       <div className='landing-page'>
         <div className='landing-div div1'>
-          <img src='./../../../img/logo_100_reversed.png'/>
+          <img style={{width: 100, height: 100}} src='./../../../img/logo_300_white.png'/>
           <div className='title-text'>
             Bevy
           </div>
           <div className='sub-title-text'>
-            The Social Network For Your Community
+            The Social Network For Your &nbsp; &nbsp;&nbsp;<span className="typedSpan"></span>
           </div>
           <div className='actions'>
             <RaisedButton
@@ -61,50 +110,79 @@ var HomeView = React.createClass({
         </div>
         <div className='landing-div div2'>
           <div className='sub-title-text'>
-            Bevy is the logical progression of modern forums
+            Unify Communication
           </div>
+          <div className='items'>
+            <div className='item'>
+              <i className='material-icons'>chat_bubble</i>
+              <div className='item-title'>
+                Chat
+              </div>
+            </div>
+            <div className='item'>
+              <i className='material-icons'>view_agenda</i>
+              <div className='item-title'>
+                Groups
+              </div>
+            </div>
+            <div className='item'>
+              <i className='material-icons'>email</i>
+              <div className='item-title'>
+                Email
+              </div>
+            </div>
+          </div>
+          <div className='bracket'>}</div>
+          <img style={{width: 100, height: 100}} src='./../../../img/logo_200.png'/>
         </div>
         <div className='landing-div div3'>
           <div className='features-title'>
-            The Feature Set Built For Your Community
+            This is How
           </div>
-          <div className='features'>
-            <div className='feature'>
-              <span className='glyphicon glyphicon-modal-window'/>
-              <div className='title'>
-                Organize.
-              </div>
-              <div className='description'>
-                Bevy organizes your community into its smaller parts
-              </div>
+          <div className='feature-section'>
+            <div className='features'>
+              <Feature 
+                title='Boards' 
+                icon={<i className='material-icons'>view_carousel</i>}
+                onClick={() => {
+                  this.setState({
+                    selected: 0
+                  })
+                }}
+                index={0}
+                selected={this.state.selected}
+                description='Organize your team into boards. Make a bevy for committees, social groups, announcements, and event feeds.'
+              />
+              <Feature
+                title='Feed'
+                icon={<i className='material-icons'>view_day</i>}
+                onClick={() => {
+                  this.setState({
+                    selected: 2
+                  })
+                }}
+                index={2}
+                selected={this.state.selected}
+                description='All recent activity in your community is easily viewed straight from your frontpage'
+              />
+              <Feature
+                title='Chat'
+                icon={<i className='material-icons'>chat_bubble</i>}
+                onClick={() => {
+                  this.setState({
+                    selected: 1
+                  })
+                }}
+                index={1}
+                selected={this.state.selected}
+                description="Each board has a group chat for all it's members, we also support group and private chat"
+              />
             </div>
-            <div className='feature'>
-              <span className='glyphicon glyphicon-user'/>
-              <div className='title'>
-                Grow.
-              </div>
-              <div className='description'>
-                Our powerful servers and well designed database can handle anything
-              </div>
-            </div>
-            <div className='feature'>
-              <span className='glyphicon glyphicon-comment'/>
-              <div className='title'>
-                Talk.
-              </div>
-              <div className='description'>
-                Each Bevy has a chat containing every member
-              </div>
-            </div>
-            <div className='feature'>
-              <span className='glyphicon glyphicon-king'/>
-              <div className='title'>
-                Enjoy.
-              </div>
-              <div className='description'>
-                Our amazing interface designers have crafted Bevy so even your grandma can use it
-              </div>
-            </div>
+            <img style={{width: 600, height: 393}} className='simplemock' src='./../../../img/simplemock.png'>
+              <div style={boardScreenStyle} className='screenshot'/>
+              <div style={chatScreenStyle} className='screenshot'/>
+              <div style={feedScreenStyle} className='screenshot'/>
+            </img>
           </div>
         </div>
         <div className='landing-div div4'/>
