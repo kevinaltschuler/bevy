d34b5bb92f82776960c7c30cc8047900f58817cb
Albert Chen <blahoink@gmail.com>
2016-02-11 19:15:11 -0500

    fix cookies across subdomains

--- middleware/cors.js
+++ middleware/cors.js
@@ -1,13 +1,22 @@
+/**
+ * middleware/cors.js
+ *
+ * lets us do stuff across domains
+ *
+ * @author albert
+ * @flow
+ */
+
 'use strict';
 
 module.exports = function(req, res, next) {
 	// TODO: restrict cors domains
 	res.header('Access-Control-Allow-Origin', '*');
-	res.header('Access-Control-Allow-Methods', 'GET, PUT, PATCH, POST, DELETE, OPTIONS');
-	res.header('Access-Control-Allow-Credentials', 'true');
-	res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, X-Requested-By, '
-		 + 'X-Access-Token, X-Revision, Content-Type, Accept, X-PINGOTHER, X-File-Name, '
-		 + 'Cache-Control, Key, Authorization, Content-Encoding');
+	res.header('Access-Control-Allow-Methods', 'HEAD, GET, PUT, PATCH, POST, DELETE, OPTIONS');
+	res.header('Access-Control-Allow-Credentials', true);
+	res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, X-Requested-By, \
+		 X-Access-Token, X-Revision, Content-Type, Accept, X-PINGOTHER, X-File-Name, \
+		 Cache-Control, Key, Authorization, Content-Encoding, X-HTTP-Method-Override');
 
 	next();
-}
+};
