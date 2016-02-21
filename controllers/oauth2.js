637913a8973dd4218a43b0b1add4aa6c0b709185
Albert Chen <blahoink@gmail.com>
2016-02-21 17:37:34 -0500

    better login

--- controllers/oauth2.js
+++ controllers/oauth2.js
@@ -13,8 +13,10 @@ var crypto = require('crypto');
 var config = require('./../config');
 var error = require('./../error');
 var shortid = require('shortid');
+var async = require('async');
 
 var User = require('./../models/User');
+var Bevy = require('./../models/Bevy');
 var AccessToken = require('./../models/AccessToken');
 var RefreshToken = require('./../models/RefreshToken');
 
@@ -81,30 +83,83 @@ server.exchange(oauth2orize.exchange.refreshToken(
 var loginUsername = function(req, res, next) {
   var username = req.body['username'];
   var password = req.body['password'];
-  User.findOne({ email: username }, function(err, user) {
-    if(err) return next(err);
-    if(!user) return next('User with that email address not found');
-    // make sure password matches
-    if(!user.verifyPassword(password)) return next('Email and password combination do not match');
+  var bevy_slug = (req.subdomains.length == 1) ? req.subdomains[0] : null;
 
-    // generate new access and refresh tokens
-    generateTokens(user, req['user'], function(err, accessToken, refreshToken, data) {
-      if(err) return next(err);
+  // only allow signing into a bevy directly.
+  if(!bevy_slug) {
+    return next('Must log in from a bevy subdomain')
+  }
+
+  async.waterfall([
+    // find the corresponding bevy (to the given subdomain/slug)
+    function(done) {
+      Bevy.findOne({ slug: bevy_slug }, function(err, bevy) {
+        if(err) return done(err);
+        // make sure the bevy we're signing into exists
+        if(!bevy) return done('Bevy with the subdomain ' + bevy_slug + ' not found');
+        // bevy found, move on
+        return done(null, bevy);
+      });
+    },
+    // find the user and verify the password
+    function(bevy, done) {
+      // find a user where their username matches either a username or an email
+      // within the specified bevy
+      var query = {
+        $and: [
+          { $or: [
+            { email: username },
+            { username: username }
+          ]},
+          { bevy: bevy._id }
+        ]
+      };
+      // send the db reqeust
+      User.findOne(query, function(err, user) {
+        if(err) return done(err);
+        // if the email doesn't match a user, then let the user know
+        if(!user) return done('User with that email address not found');
+        // make sure password matches
+        if(!user.verifyPassword(password)) return done('Email and password combination do not match');
+        // move on
+        return done(null, user);
+      });
+    },
+    // generate oauth2 tokens
+    function(user, done) {
+      // generate new access and refresh tokens
+      generateTokens(
+        user, // the user object
+        req['user'], // the client object (stored in user for some reason)
+        // and the callback
+        function(err, accessToken, refreshToken, data) {
+          if(err) return done(err);
+          else return done(null, {
+            accessToken: accessToken,
+            refreshToken: refreshToken,
+            user: user.toJSON(),
+            expires_in: data['expires_in']
+          });
+        }
+      );
+    },
+    // log the user into passport
+    function(results, done) {
       // clear serialized client
       //req.logout();
       // serialize user into session
       // and pass access & refresh tokens for the client
       // also give client expiration date so it can request new token by itself
-      req.logIn(user, function(err) {
-        if(err) return next(err);
-        return res.json({
-          accessToken: accessToken,
-          refreshToken: refreshToken,
-          user: user.toJSON(),
-          expires_in: data['expires_in']
-        });
+      req.logIn(results.user, function(err) {
+        if(err) return done(err);
+        else return done(null, results);
       });
-    });
+    }
+  ], function(err, result) {
+    // 500 any errors inside the watefall functions
+    if(err) return next(err);
+    // else return the tokens and signed in user
+    else return res.json(result);
   });
 };
 
