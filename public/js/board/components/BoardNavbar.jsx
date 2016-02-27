a9a5903f22d778979f5ad36bf81bc5463b9c78af
Albert Chen <blahoink@gmail.com>
2016-02-26 22:33:46 -0500

    clean up board navbar

--- public/js/board/components/BoardNavbar.jsx
+++ public/js/board/components/BoardNavbar.jsx
@@ -2,8 +2,9 @@
  * BoardNavbar.jsx
  *
  * the navbar for each board
- * 
- * @author  kevin
+ *
+ * @author kevin "bugwriter brown" altschuler
+ * @author albert
  * @flow
  */
 
@@ -12,34 +13,46 @@
 var React = require('react');
 var Ink = require('react-ink');
 
+var _ = require('underscore');
+var constants = require('./../../constants');
+var router = require('./../../router');
+
 var BoardNavbar = React.createClass({
   propTypes: {
-    activeBoard: React.PropTypes.object
+    activeBoard: React.PropTypes.object,
+    sidebarOpen: React.PropTypes.bool
+  },
+
+  renderInfoIcon() {
+    if(this.props.sidebarOpen) {
+      return <i style={{color: '#2cb673'}} className="material-icons">info_outline</i>;
+    } else {
+      return <i className="material-icons">info_outline</i>;
+    }
   },
 
   render() {
-    var infoIcon = (this.props.sidebarOpen) 
-      ? <i style={{color: '#2cb673'}} className="material-icons">info_outline</i> 
-      : <i className="material-icons">info_outline</i>;
-    var buttonStyle = (this.props.sidebarOpen)
-    ?  {boxShadow: 'inset 0 1px 2px 0 rgba(0,0,0,.075)'}
-    :  {};
-    var board = this.props.activeBoard;
     return (
-        <div className='board-navbar'>
-          <div className='left'>
-            <div className='title'>
-              {board.name}
-            </div>  
-          </div>
-          <div className='right'>
-            <button style={buttonStyle} className='info-button' onClick={this.props.toggleSidebar}>
-              <Ink/>
-              {infoIcon}
-            </button>
+      <div className='board-navbar'>
+        <div className='left'>
+          <div className='title'>
+            { this.props.activeBoard.name }
           </div>
         </div>
-      );
+        <div className='right'>
+          <button
+            style={(this.props.sidebarOpen)
+              ? { boxShadow: 'inset 0 1px 2px 0 rgba(0,0,0,.075)' }
+              : {}}
+            className='info-button'
+            onClick={ this.props.toggleSidebar }
+          >
+            <Ink/>
+            { this.renderInfoIcon() }
+          </button>
+        </div>
+      </div>
+    );
   }
 
 });
